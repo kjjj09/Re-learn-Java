365beb5809a60e66d4707a556a44fc09698c272a
MUJUN <MUJUN@MJ>
2022-04-10 23:15:06 +0900

    2022_04_10_2315_Variable

--- Basic/src/example/variable/Run.java
+++ Basic/src/example/variable/Run.java
@@ -5,7 +5,8 @@ public class Run {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Variable var = new Variable();
-		var.initVariable();
+		//var.initVariable();
+		var.changeValue();
 	}
 
 }
