26701b3bc33ad87a4636f966aae18e909e352f2f
MUJUN <MUJUN@MJ>
2022-04-10 23:13:58 +0900

    Revert "2022_04_10_23_10_Variable"
    This reverts commit 9dd39ac5ddc6ec0a299d482d1794892b1a2e7034.

--- Basic/src/example/variable/Run.java
+++ Basic/src/example/variable/Run.java
@@ -5,8 +5,7 @@ public class Run {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Variable var = new Variable();
-		//var.initVariable();
-		var.changeValue();
+		var.initVariable();
 	}
 
 }
